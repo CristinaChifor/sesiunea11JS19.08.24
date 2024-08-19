3d40d9f6bbae864dc87016f0b8cb26e1d9f6fb06
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-19 23:32:09 +0300

    Typeof operator

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+// Typeof operator
+
+let x = 5;
+console.log(typeof x);
+
+let str = "Hello";
+console.log(typeof str);
+
+let isTrue = true;
+console.log(typeof isTrue);
+
+let undefinedValue;
+console.log(typeof undefinedValue);
+
+// Array & Date
